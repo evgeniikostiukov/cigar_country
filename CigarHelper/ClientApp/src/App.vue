733b48e5f04670231aa04c37b0fb0280e9875e5a
Eugene Kostyukov <evgenii.kostiukov@gmail.com>
2025-01-21 00:53:03 +0300

    Поправил скролл на странице

--- CigarHelper/ClientApp/src/App.vue
+++ CigarHelper/ClientApp/src/App.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
+import AlertAdult from '@/components/AlertAdult.vue';
 import HeaderView from '@/views/HeaderView.vue';
 import { ConfirmDialog, Toast } from 'primevue';
 import { RouterView } from 'vue-router';
-import AlertAdult from '@/components/AlertAdult.vue';
-import "./assets/base.css"
+import './assets/base.css'; /* PartiallyEnd: #3632/scriptSetup.vue */
 </script>
 
 <template>
-
   <AlertAdult />
   <HeaderView />
-  <div class="container-fluid">
+  <div class="container-fluid w-full">
     <div id="mainSize" class="flex flex-row">
       <div class="col h-full w-full">
         <RouterView v-slot="{ Component }">
@@ -27,9 +26,8 @@ import "./assets/base.css"
 
 <style scoped lang="postcss">
 #mainSize {
-  /*header + footer + border*/
-  max-height: calc(100vh - 60px);
-  min-height: calc(100vh - 60px);
   height: calc(100vh - 60px);
+  min-height: calc(100vh - 60px);
+  max-height: calc(100vh - 60px);
 }
 </style>
